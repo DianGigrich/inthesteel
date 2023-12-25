d2a31a91bdbd5bb84a9790d498a49f995901feac
Dian Sherman <slayer_barrett_@hotmail.com>
2023-12-24 18:22:41 -0800

    asdf

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Box, Typography, Container } from '@mui/material';
 
 export default function Fence(props) {
-
+  const [newItem, setNewItem] = useState("")
+  // fix the todos and tasks
+      useEffect(()=>{
+          API.getUserFence(props.userId).then(data=>{
+              console.log(data)
+              setTasks(data.Items)
+          })
+      },[props.userId])
   return (
     <Box sx={{
       pt: 8,
